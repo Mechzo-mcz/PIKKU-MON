a0cd520246c66059f061ec3540c9d3179bd2e858
Mechzo-mcz <92770902+Mechzo-mcz@users.noreply.github.com>
2021-10-19 11:04:28 +0530

    Create config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
     VERSION: 'v8.0.0',
     CHANNEL: 'https://t.me/remasterplugin',
-    SESSION: process.env.LIZA_SESSION === undefined ? '' : process.env.LIZA_SESSION,
+    SESSION: process.env.PIKKU_SESSION === undefined ? '' : process.env.PIKKU_SESSION,
     ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
     AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,
     GANSTYLE: process.env.GAN_IMAGE === undefined ? 'https://i.hizliresim.com/loUtAb.jpg' : process.env.GAN_IMAGE,
@@ -30,7 +30,7 @@ module.exports = {
     BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
     WELCOME: process.env.WELCOME === undefined ? 'pp' : process.env.WELCOME,
     OWNER: process.env.OWNER_NAME === undefined ? 'default' : process.env.OWNER_NAME,
-    ALL: process.env.ALL_CAPTION === undefined ? 'Made By LIZA MWOL' : process.env.ALL_CAPTION,
+    ALL: process.env.ALL_CAPTION === undefined ? 'Made By PIKKU MON' : process.env.ALL_CAPTION,
     MENTION: process.env.TAG_REPLY === undefined ? '918921483992@s.whatsapp.net' : process.env.TAG_REPLY,
     ADDMSG: process.env.ADD_MESSAGE === undefined ? 'default' : process.env.ADD_MESSAGE,
     PLKS: process.env.THERI_LIST === undefined ? false : process.env.THERI_LIST,
@@ -39,11 +39,11 @@ module.exports = {
     DISBGM: process.env.DISABLE_JID_BGM_FILTER === undefined ? false : process.env.DISABLE_JID_BGM_FILTER,
     STICKERP: process.env.AUTO_STICKER === undefined ? true : convertToBool(process.env.AUTO_STICKER),
     DISSTICKER: process.env.DISABLE_STICKER === undefined ? false : process.env.DISABLE_STICKER,
-    BOTLIZA: process.env.BOT_NAME === undefined ? '  𝐋𝐈𝐙𝐀 𝐌𝐖𝐎𝐋࿐  \n\n   *ωнαтsαρρ вσт™*   \n' : process.env.BOT_NAME,
+    BOTLIZA: process.env.BOT_NAME === undefined ? '  *ᴘɪᴋᴋᴜ ᴍᴏɴ*  \n\n   *ωнαтsαρρ вσт™*   \n' : process.env.BOT_NAME,
     NOLOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
     THERI_KICK: process.env.THERI_KICK === undefined ? 'false' : process.env.THERI_KICK,
     SONGD: process.env.SONGD === undefined ? '*╔══╗ ♫*\n*║██║ ♪♪*\n*║██║♫♪*\n*║ ◎♫♪♫ ✎﹏sᴇᴀʀᴄʜɪɴɢ﹏﹏﹏﹏𝅘𝅥𝅮𝅘𝅥𝅯𝆺𝅥𝅯𝄞﹏* \n*╚══╝*' : process.env.SONGD,
-    SONGU: process.env.SONGU === undefined ? '🔉⃝🔊𓊈𒆜UPLOADING SONG𒆜𓊉࿐\n   🌹⃟⃢👑❦𝙇𝙄𝙕𝘼 𝙈𝙒𝙊𝙇❦🌹⃟⃢♘*~\n0:35 ━━●────────── -5:33\n⇆        ◁ㅤㅤ ❚❚ㅤㅤ ▷        ↻\n      VOLUME 🔉■■■■□90%🔊\n     🧚🏼‍♂️✶⊶⊷⊶⊷❍⊶⊷⊶⊷✶🧚🏼‍♂️' : process.env.SONGU,
+    SONGU: process.env.SONGU === undefined ? '🔉⃝🔊𓊈𒆜UPLOADING SONG𒆜𓊉࿐\n   🌹⃟⃢👑❦𝕡𝕚𝕜𝕜𝕦 𝕞𝕠𝕟❦🌹⃟⃢♘*~\n0:35 ━━●────────── -5:33\n⇆        ◁ㅤㅤ ❚❚ㅤㅤ ▷        ↻\n      VOLUME 🔉■■■■□90%🔊\n     🧚🏼‍♂️✶⊶⊷⊶⊷❍⊶⊷⊶⊷✶🧚🏼‍♂️' : process.env.SONGU,
     FULLEVA: process.env.FULL_EVA === undefined ? 'false' : process.env.FULL_EVA,
     BLOCKMSG: process.env.BLOCK_MESSAGE === undefined ? 'default' : process.env.BLOCK_MESSAGE,
     UNBLOCKMSG: process.env.UNBLOCK_MESSAGE === undefined ? 'default' : process.env.UNBLOCK_MESSAGE,
@@ -75,7 +75,7 @@ module.exports = {
     SUDO: process.env.SUDO === undefined ? false : process.env.SUDO,
     DEBUG: DEBUG,
     WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG",
-    SUPPORT: "905524317852-1612300121",
-    SUPPORT2: "905511384572-1617736751",
-    SUPPORT3: "905511384572-1621015274"
+    SUPPORT: "98086567984-1612300121",
+    SUPPORT2: "90000000000-1617736751",
+    SUPPORT3: "90000000000-1621015274"
 };
